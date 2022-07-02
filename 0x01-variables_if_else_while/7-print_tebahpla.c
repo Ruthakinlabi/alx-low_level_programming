63ed51467eef5149fe68a0e40bc785e68136a474
Ruthakinlabi <akinlabifolashade47@gmail.com>
2022-07-01 17:46:41 -0700

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,8 +8,11 @@
 int main(void)
 {
 int num;
+char letter;
 for (num = 0; num < 10; num++)
-printf("%d", num);
-printf("\n");
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
 return (0);
 }
