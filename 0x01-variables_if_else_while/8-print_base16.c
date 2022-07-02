a6d02b7428ccd92456795665515964cad60b4eb1
Ruthakinlabi <akinlabifolashade47@gmail.com>
2022-07-01 17:57:56 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,14 @@
 int main(void)
 {
 int num;
-char letter;
-for (num = 0; num < 10; num++)
+for (num = 0; num <= 9; num++)
+{
 putchar((num % 10) + '0');
-for (letter = 'a'; letter <= 'f'; letter++)
-putchar(letter);
+if (num == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
 putchar('\n');
 return (0);
 }
