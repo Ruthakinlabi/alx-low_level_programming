de35bf8819350a72861f0c3b27fc8e098ea6daed
Ruthakinlabi <akinlabifolashade47@gmail.com>
2022-07-01 18:44:54 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,14 @@
 */
 int main(void)
 {
-int digit1, digit2;
-for (digit1 = 0; digit1 < 9; digit1++)
+int num;
+for (num = 0; num <= 9; num++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-{
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
-if (digit1 == 8 && digit2 == 9)
+putchar((num % 10) + '0');
+if (num == 9)
 continue;
 putchar(',');
 putchar(' ');
-}
 }
 putchar('\n');
 return (0);
